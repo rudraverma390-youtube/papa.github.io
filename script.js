959d315afcb10135d809c1374826ae9494c616ea
rudraverma390-youtube <80309555+rudraverma390-youtube@users.noreply.github.com>
2021-09-26 00:04:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter UserName");
+ var b=prompt("Enter password");
+  	if(a== "Rajiv Kumar Verma" && b=="26-09-1971")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
